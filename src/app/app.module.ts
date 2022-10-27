e3c088c7ac0ff6ad6517614761f786d0e03d27dd
Vanessa <machado.vanessa32@gmail.com>
2022-10-27 14:06:44 -0300

    Add estilo no botão e import necessario para o material funcionar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,9 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailsComponent } from './hero-details/hero-details.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 import { MessagesComponent } from './messages/messages.component';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
   declarations: [
@@ -22,6 +24,8 @@ import { MessagesComponent } from './messages/messages.component';
     FormsModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatButtonModule,
+    MatExpansionModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -26,7 +26,7 @@ export class HeroesComponent  implements OnInit {
   
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-    this.messagesService.add('HeroesComponent: Selected hero id=' + hero.id);
+    this.messagesService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
 }
